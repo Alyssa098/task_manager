f1ccf47c4c4b8dd24c1ada8f6aedbc5425f5ac1d
“Alyssa <lasher1010@hotmail.com>
2014-09-22 10:17:54 -0500

    Former login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,9 +14,10 @@ class ApplicationController < ActionController::Base
   
   def current_user
       if session[:user_id]
-      @current_user ||= User.find_by_id(session[:user_id])
+      #@current_user ||= User.find_by_id(session[:user_id])
+      @current_user ||= User.find(session[:user_id])
       if @current_user.nil?
-        reset_session 
+        #reset_session 
         redirect_to login_path, :alert => "You need to log in first!"
       end
     end
